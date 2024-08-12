4f5740f8b3186ef8c93a8c9b6b23d3c9bf031a87
Astrum93 <jungmin2670@gmail.com>
2024-08-12 21:29:38 +0900

    [refactor] 주석 추가
    한 눈에 알아보기 쉽게 주석 추가

--- lib/service/github_api/github_api_service.dart
+++ lib/service/github_api/github_api_service.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 class GithubApiService {
+  /// 유저 정보를 불러 오는 메서드
   static Future getUserInfos() async {
     final dio = Dio();
     try {
