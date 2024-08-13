ce81d8e9e089b674e54959e4549101d2d30c3deb
Astrum93 <jungmin2670@gmail.com>
2024-08-13 23:02:28 +0900

    [test] 'link' 헤더를 참고하여 여러 테스트 진행
    - 어떤 파라미터를 줘야하는지 테스트 진행
    - 'page', 'per_page'와 관계없이 처음 불러온 url에서 나온 Link 헤더의 rel = "next"에 부합하는 'since' 파라미터값이 다음 데이터를 가져온다는 사실 발견.

--- lib/service/github_api/github_api_service.dart
+++ lib/service/github_api/github_api_service.dart
@@ -6,7 +6,27 @@ class GithubApiService {
   static Future getUserInfos() async {
     final dio = Dio();
     try {
-      final response = await dio.get('https://api.github.com/users');
+      final response = await dio.get(
+        'https://api.github.com/users',
+        queryParameters: {
+          'since': 900,
+        },
+      );
+      debugPrint(response.headers['link'].toString());
+      debugPrint(response.headers['link']?[0]);
+      if (response.headers['link']![0].contains('rel="prev"')) {
+        debugPrint('prev 페이지가 있습니다.');
+      }
+      if (response.headers['link']![0].contains('rel="next"')) {
+        debugPrint('next 페이지가 있습니다.');
+      }
+      if (response.headers['link']![0].contains('rel="last"')) {
+        debugPrint('last 페이지가 있습니다.');
+      }
+      if (response.headers['link']![0].contains('rel="first"')) {
+        debugPrint('first 페이지가 있습니다.');
+      }
+
       return response;
     } on DioException catch (e) {
       if (e.type == DioExceptionType.badResponse) {
