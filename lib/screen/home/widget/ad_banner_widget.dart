ddfac08f9fce5718eef7e24f7a4bfd6eac1e5f8f
Astrum93 <jungmin2670@gmail.com>
2024-08-14 23:13:03 +0900

    [feat] 광고 이미지 터치시 인터넷 브라우저 이동
    광고 배너 영역 터치 시 'https://taxrefundgo.kr' 주소로 이동

--- lib/screen/home/widget/ad_banner_widget.dart
+++ lib/screen/home/widget/ad_banner_widget.dart
@@ -1,19 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class AdBanner extends StatelessWidget {
+class AdBanner extends StatefulWidget {
   const AdBanner({
     super.key,
   });
 
   @override
+  State<AdBanner> createState() => _AdBannerState();
+}
+
+class _AdBannerState extends State<AdBanner> {
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: 100,
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: NetworkImage(
-            'https://placehold.it/500x100?text=ad',
+    return GestureDetector(
+      onTap: () {
+        launchUrl(Uri.parse('https://taxrefundgo.kr'));
+      },
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 100,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage(
+              'https://placehold.it/500x100?text=ad',
+            ),
           ),
         ),
       ),
