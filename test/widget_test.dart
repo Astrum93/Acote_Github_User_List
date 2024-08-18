41fc90e8b359b876c19b891d27611d36c7ada7c0
Astrum93 <jungmin2670@gmail.com>
2024-08-19 02:07:47 +0900

    [feat] AdBanner Widget Test 추가
    * Image가 잘 나오는지 테스트
    * GestureDetector가 잘 되는지 테스트

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,21 +1,36 @@
+import 'package:acote_github_user_list_app/screen/home/widget/ad_banner_widget.dart';
 import 'package:acote_github_user_list_app/screen/home/widget/user_profile_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('User Profile Widget Test', (tester) async {
-    String userId = 'testName';
-    String userAvatarUrl = 'https://example.com/testName.png';
-    await tester.pumpWidget(
-      MaterialApp(
-        home: Scaffold(
-          body: UserProfile(userId: userId, userAvatarUrl: userAvatarUrl),
+  group('HomeScreen Widget Test', () {
+    testWidgets('User Profile Widget Test', (tester) async {
+      String userId = 'testName';
+      String userAvatarUrl = 'https://example.com/testName.png';
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: UserProfile(userId: userId, userAvatarUrl: userAvatarUrl),
+          ),
         ),
-      ),
-    );
-    expect(find.text(userId), findsOneWidget);
-    expect(find.byType(CircularProgressIndicator), findsNothing);
-    expect(find.byType(CircleAvatar), findsOneWidget);
-    expect(find.byType(Image), findsOneWidget);
+      );
+      expect(find.text(userId), findsOneWidget);
+      expect(find.byType(CircularProgressIndicator), findsNothing);
+      expect(find.byType(CircleAvatar), findsOneWidget);
+      expect(find.byType(Image), findsOneWidget);
+    });
+    testWidgets('AdBanner Widget Test', (tester) async {
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: Scaffold(
+            body: AdBanner(),
+          ),
+        ),
+      );
+      expect(find.byType(Image), findsOneWidget);
+      expect(find.byType(GestureDetector), findsOneWidget);
+      await tester.tap(find.byType(GestureDetector));
+    });
   });
 }
