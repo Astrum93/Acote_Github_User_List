ae793b752f8ecef12722cb768e12bdbdd487a7da
Astrum93 <jungmin2670@gmail.com>
2024-08-19 01:29:15 +0900

    [feat] User Profile Widget Test 추가
    * userId가 올바르게 표시되는지 테스트
    * userAvatarUrl이 비어있는 경우 CircularProgressIndicator가 실행되는지 테스트
    * CircleAvatar가 표시되는지 테스트
    * userAvatarUrl이 어떻게 제공되는지에 따라 위젯트리에 Image가 생성되는지 테스트

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,27 +1,21 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-
+import 'package:acote_github_user_list_app/screen/home/widget/user_profile_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  // testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-  //   // Build our app and trigger a frame.
-  //   await tester.pumpWidget(const MyApp());
-  //
-  //   // Verify that our counter starts at 0.
-  //   expect(find.text('0'), findsOneWidget);
-  //   expect(find.text('1'), findsNothing);
-  //
-  //   // Tap the '+' icon and trigger a frame.
-  //   await tester.tap(find.byIcon(Icons.add));
-  //   await tester.pump();
-  //
-  //   // Verify that our counter has incremented.
-  //   expect(find.text('0'), findsNothing);
-  //   expect(find.text('1'), findsOneWidget);
-  // });
+  testWidgets('User Profile Widget Test', (tester) async {
+    String userId = 'testName';
+    String userAvatarUrl = 'https://example.com/testName.png';
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: UserProfile(userId: userId, userAvatarUrl: userAvatarUrl),
+        ),
+      ),
+    );
+    expect(find.text(userId), findsOneWidget);
+    expect(find.byType(CircularProgressIndicator), findsNothing);
+    expect(find.byType(CircleAvatar), findsOneWidget);
+    expect(find.byType(Image), findsOneWidget);
+  });
 }
