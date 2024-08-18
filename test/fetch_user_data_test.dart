19a22262971785dee465cf8645725de829301078
Astrum93 <jungmin2670@gmail.com>
2024-08-19 00:59:54 +0900

    [feat] User Repos Data Fetch Test 추가
    * User Repos Data Fetch Test 추가 작성
    * 실제 사용중인 GithubApiService의 메서드들을 테스트를 고려하지 않고 작성하여 새롭게 fetchUserReposData 생성

--- test/fetch_user_data_test.dart
+++ test/fetch_user_data_test.dart
@@ -7,35 +7,80 @@ import 'fetch_user_data_test.mocks.dart';
 
 @GenerateMocks([Dio])
 void main() {
-  test('Github User Data Fetch', () async {
+  group('GithubApi Test', () {
     final dio = MockDio();
-    final mockData = [
-      {
-        'login': 'user1',
-        'avatar_url': 'https://example.com/user1.png',
-        'repos_url': 'https://api.github.com/users/user1/repos'
-      },
-      {
-        'login': 'user2',
-        'avatar_url': 'https://example.com/user2.png',
-        'repos_url': 'https://api.github.com/users/user2/repos'
-      }
-    ];
-    when(dio.get('https://api.github.com/users')).thenAnswer(
-      (_) async => Future.value(
-        Response(
-            data: mockData,
+
+    /// User Data Fetch Test
+    test('Github User Data Fetch', () async {
+      final mockData = [
+        {
+          'login': 'user1',
+          'avatar_url': 'https://example.com/user1.png',
+          'repos_url': 'https://api.github.com/users/user1/repos'
+        },
+        {
+          'login': 'user2',
+          'avatar_url': 'https://example.com/user2.png',
+          'repos_url': 'https://api.github.com/users/user2/repos'
+        }
+      ];
+      when(dio.get('https://api.github.com/users')).thenAnswer(
+        (_) async => Future.value(
+          Response(
+              data: mockData,
+              statusCode: 200,
+              requestOptions: RequestOptions(path: '')),
+        ),
+      );
+      final response = await fetchUserData(dio);
+      expect(response.data, mockData);
+    });
+
+    /// User Repos Data Fetch Test
+    test('Fetch Github User Repos Data Fetch', () async {
+      final mockRepoData = [
+        {
+          'name': 'repo1',
+          'description': 'This is repo1',
+          'stargazers_count': 100,
+          'language': 'Dart',
+          'private': false,
+        },
+        {
+          'name': 'repo2',
+          'description': 'This is repo2',
+          'stargazers_count': 200,
+          'language': 'Flutter',
+          'private': true,
+        }
+      ];
+
+      when(dio.get('https://api.github.com/users/mojombo/repos')).thenAnswer(
+        (_) async => Future.value(
+          Response(
+            data: mockRepoData,
             statusCode: 200,
-            requestOptions: RequestOptions(path: '')),
-      ),
-    );
-    final response = await fetchUserData(dio);
-    expect(response.data, mockData);
+            requestOptions: RequestOptions(
+                path: 'https://api.github.com/users/mojombo/repos'),
+          ),
+        ),
+      );
+
+      final response = await fetchUserReposData(dio);
+      expect(response.data, mockRepoData);
+    });
   });
 }
 
 Future fetchUserData(Dio dio) async {
   final response = await dio.get('https://api.github.com/users');
+  if (response.statusCode == 200) {
+    return response;
+  }
+}
+
+Future fetchUserReposData(Dio dio) async {
+  final response = await dio.get('https://api.github.com/users/mojombo/repos');
   if (response.statusCode == 200) {
     return response;
   }
